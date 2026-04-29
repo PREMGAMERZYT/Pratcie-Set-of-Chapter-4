9b00d7aefd8e27d7cbd2de79a6ba1ebc14fdba9d
PremCoder <kasturawwashunnamani@gmail.com>
2026-04-29 20:08:34 +0530

    update

--- app/src/main/java/com/example/pratciesetofchaper4/MainActivity.java
+++ app/src/main/java/com/example/pratciesetofchaper4/MainActivity.java
@@ -16,6 +16,6 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            Insets systemBars = insets.getInsets
     }
 }
