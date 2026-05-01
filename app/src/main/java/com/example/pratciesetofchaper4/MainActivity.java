29e2ede8162fc72b6863d0d51d31b14f27323eb0
PremCoder <kasturawwashunnamani@gmail.com>
2026-05-01 12:42:26 +0530

    added  the toast On button to test the clicking

--- app/src/main/java/com/example/pratciesetofchaper4/MainActivity.java
+++ app/src/main/java/com/example/pratciesetofchaper4/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.pratciesetofchaper4;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +13,26 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button button;
+    private Button button2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        button.findViewById(R.id.button);
+        button2.findViewById(R.id.button2);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "No", Toast.LENGTH_SHORT).show();
+            }
+        });
+
 
     }
 }
