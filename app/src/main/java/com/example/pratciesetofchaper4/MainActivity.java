052e26324a33c72ef81828728f157f890522be3f
PremCoder <kasturawwashunnamani@gmail.com>
2026-05-02 16:01:12 +0530

    added the Logic behind the 3rd question

--- app/src/main/java/com/example/pratciesetofchaper4/MainActivity.java
+++ app/src/main/java/com/example/pratciesetofchaper4/MainActivity.java
@@ -3,12 +3,13 @@ package com.example.pratciesetofchaper4;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-import java.util.random.;
 
+import java.util.Random;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -25,6 +26,13 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Random rand = new Random();
+
+        for (int i = 0; i <=10; i++) {
+            int randomintehger  = rand.nextInt(100);
+            Log.d("Prem", String.valueOf(randomintehger) );
+        }
 
 
     }
