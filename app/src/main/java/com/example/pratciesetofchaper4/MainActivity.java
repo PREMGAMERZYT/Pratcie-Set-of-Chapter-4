63a70e7576e39443b03e43fa07afb237a86cdfb3
PremCoder <kasturawwashunnamani@gmail.com>
2026-05-02 15:49:31 +0530

    Solving The Second Question

--- app/src/main/java/com/example/pratciesetofchaper4/MainActivity.java
+++ app/src/main/java/com/example/pratciesetofchaper4/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import java.util.random.;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
