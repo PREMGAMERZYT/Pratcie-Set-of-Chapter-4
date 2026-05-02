5d54bdef29c8bffe1d6f1bfa4a743d0adec0d67a
PremCoder <kasturawwashunnamani@gmail.com>
2026-05-02 15:50:32 +0530

    removed all the previus question code

--- app/src/main/java/com/example/pratciesetofchaper4/MainActivity.java
+++ app/src/main/java/com/example/pratciesetofchaper4/MainActivity.java
@@ -25,23 +25,7 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button = findViewById(R.id.button);
-        editText = findViewById(R.id.editText);
 
-        button.setOnClickListener(v -> {
-            String urltext = editText.getText().toString();
-            if(urltext.isEmpty()) {
-                Toast.makeText(this, "Please Enter The url", Toast.LENGTH_SHORT).show();
-            } else if (!urltext.startsWith("http://") && !urltext.startsWith("https://")) {
-                urltext = "https://" + urltext;
-            }
-            Uri webpage = Uri.parse(urltext);
-            
-            Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
-            Intent chosser = Intent.createChooser(intent, "Open With");
-            startActivity(chosser);
-
-        });
 
     }
 }
