543cee390fa4f0f4776e66eada15e8dfe0ae5d18
PremCoder <kasturawwashunnamani@gmail.com>
2026-05-05 19:21:03 +0530

    added the second activity logit

--- app/src/main/java/com/example/pratciesetofchaper4/MainActivity2.java
+++ app/src/main/java/com/example/pratciesetofchaper4/MainActivity2.java
@@ -19,55 +19,16 @@ public class MainActivity2 extends AppCompatActivity {
 
     private TextView textView;
 
-    private EditText address;
-
-    private EditText subject;
-
-    private EditText message;
-
-    private Button send;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        int ra = getIntent().getIntExtra("raandnumber", 0);
-        String congrts = "Congrulation You Won The Number Was = " + ra;
+        int score = getIntent().getIntExtra("score",0);
         textView = findViewById(R.id.textView2);
-        textView.setText(congrts);
+        String first = textView.getText().toString();
+        String Second = first + score;
+        textView.setText(Second);
 
-        address = findViewById(R.id.address);
-        subject = findViewById(R.id.subject);
-        message = findViewById(R.id.message);
-        send = findViewById(R.id.send);
-
-
-        send.setOnClickListener(v -> {
-            String Address = address.getText().toString();
-            String Subject = subject.getText().toString();
-            String Message = message.getText().toString();
-
-            if (Address.isEmpty()) {
-                Toast.makeText(this, "Please Enter The Email", Toast.LENGTH_SHORT).show();
-                return;
-            }
-
-            composeEmail(Address,Subject,Message);
-
-        });
-    }
-
-    public void composeEmail(String addresses, String subject, String attachment) {
-        Intent intent = new Intent(Intent.ACTION_SEND);
-        intent.setType("*/*");
-        intent.putExtra(Intent.EXTRA_EMAIL, addresses);
-        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
-        intent.putExtra(Intent.EXTRA_STREAM, attachment);
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            startActivity(intent);
-        } else {
-            Toast.makeText(this, "Email App not found", Toast.LENGTH_SHORT).show();
-        }
     }
 
 
